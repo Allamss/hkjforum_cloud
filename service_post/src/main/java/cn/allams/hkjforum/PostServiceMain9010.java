92d721c630742deabd3ca57bfea7e7f910b79b8b
company <522458390@qq.com>
2020-06-02 14:26:17 +0800

    帖子回复服务主启动

---
+++ service_post/src/main/java/cn/allams/hkjforum/PostServiceMain9010.java
@@ -0,0 +1,16 @@
+package cn.allams.hkjforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 帖子回复服务主启动类
+ *
+ * @author Allams
+ */
+@SpringBootApplication
+public class PostServiceMain9010 {
+    public static void main(String[] args) {
+        SpringApplication.run(PostServiceMain9010.class, args);
+    }
+}
