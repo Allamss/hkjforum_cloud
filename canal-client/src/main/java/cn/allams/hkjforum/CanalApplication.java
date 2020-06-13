6f91ea905b7ba20ef82efdab09eebb9fb90d78f3
Allamss <522458390@qq.com>
2020-06-13 20:32:46 +0800

    Canal同步模块完成

---
+++ canal-client/src/main/java/cn/allams/hkjforum/CanalApplication.java
@@ -0,0 +1,28 @@
+package cn.allams.hkjforum;
+
+import cn.allams.hkjforum.client.CanalClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Allams
+ */
+@SpringBootApplication
+public class CanalApplication implements CommandLineRunner {
+
+    @Resource
+    private CanalClient canalClient;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        //项目启动，执行canal客户端监听
+        canalClient.run();
+    }
+}
