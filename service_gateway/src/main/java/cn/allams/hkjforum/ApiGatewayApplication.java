64a7eafe8e647caac36df6a969f9a1f70a5cac83
Allamss <522458390@qq.com>
2020-06-14 20:28:38 +0800

    gateway API网关模块完成

---
+++ service_gateway/src/main/java/cn/allams/hkjforum/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package cn.allams.hkjforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Allams
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
---
+++ service_gateway/src/main/java/cn/allams/hkjforum/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package cn.allams.hkjforum.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author Allams
+ * 注意，因为在网关这配置了跨域，经过该网关的微服务API controller都不需要@CrossOrign
+ * 如果有了，访问时会有多重跨域错误
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
