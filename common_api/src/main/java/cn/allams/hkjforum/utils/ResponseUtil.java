69ff1fcc5d346b76c802456f1baf38ed126672af
Allamss <522458390@qq.com>
2020-06-15 18:13:51 +0800

    整合Spring Security权限框架

---
+++ common_api/src/main/java/cn/allams/hkjforum/utils/ResponseUtil.java
@@ -0,0 +1,25 @@
+package cn.allams.hkjforum.utils;
+
+import cn.allams.hkjforum.entity.CommonResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Allams
+ */
+public class ResponseUtil {
+    public static void out(HttpServletResponse response, CommonResult r) {
+        ObjectMapper mapper = new ObjectMapper();
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        try {
+            mapper.writeValue(response.getWriter(), r);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
